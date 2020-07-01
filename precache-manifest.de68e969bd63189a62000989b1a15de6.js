5428208f51f583c54892e830a1c17d97f1eaaad2
0xAubrey <0xAubrey@users.noreply.github.com>
2020-07-01 15:44:33 +0100

    Updates

---
+++ precache-manifest.de68e969bd63189a62000989b1a15de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201bfe0b9486dbafd5994090dacd4c61",
+    "url": "/scipio/index.html"
+  },
+  {
+    "revision": "10cad940c52e5cec1890",
+    "url": "/scipio/static/css/main.8b2f572e.chunk.css"
+  },
+  {
+    "revision": "f7ecb3c4d2fdafe86d5e",
+    "url": "/scipio/static/js/2.4c27f118.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scipio/static/js/2.4c27f118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cad940c52e5cec1890",
+    "url": "/scipio/static/js/main.e753968f.chunk.js"
+  },
+  {
+    "revision": "16c53bb31000578ac40a",
+    "url": "/scipio/static/js/runtime-main.d7ef34d7.js"
+  }
+]);
